72be4af1bb02a3b7deb6d4426239f7c1e8b1c711
I1538 <I1538@ISILLT10372.indium.com>
2019-06-21 13:16:35 +0530

    Modified file

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Hello World program");
+		System.out.println("First GIT Commit Check");
 	}
 
 }
